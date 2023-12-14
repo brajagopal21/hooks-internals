ab1c29f12c13aeba2de523db34f8d8af9203ba33
brajagopal21 <brajagopalmukherjee21@gmail.com>
2023-12-14 14:27:40 +0530

    usestate and effect

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # hooks-internals
 
 [Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/brajagopal21/hooks-internals)
+
+Hooks are stateful functions-->syncronization 
+
+normal function cann't remember ,hooks does
+
+usestate is cleared automatically
+
+useEffect basically to retrive data 
+
+
--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useState,useEffect } from 'react';
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+
+  useEffect(()=>{
+    console.log("hello");
+    document.addEventListener('click',handle);
+    return ()=>{
+      document.removeEventListener('click',handle);
+    }
+  },[]);
+
+  function handleMinus() {
+    setCount(count - 1);
+  }
+
+  function handlePlus() {
+    setCount(count + 1);
+  }
+
   return (
-    <h1>Hooks Internals</h1>
-  );
+    <>
+      <h1>Hooks Internals</h1>
+      <button onClick={handleMinus}>Minus</button>
+      <span>{count}</span>
+      <button onClick={handlePlus}>Plus</button>
+    </>
+  )
 }
